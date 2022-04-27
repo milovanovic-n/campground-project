c064239fdb379b569df359dbca17a50b456ce505
Nemanja <nemanja94email@gmail.com>
2022-04-27 10:36:52 +0200

    add route to delete a review

--- index.js
+++ index.js
@@ -113,6 +113,13 @@ app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async(req, res, 
   await review.save();
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async(req, res, next) => {
+  const {id, reviewId} = req.params;
+  await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all("*", (req, res, next) => {
